f9ebc48a7eff1fa4baf27bf89dde73213ddbdf9e
David Wert <david.j.wert@gmail.com>
2017-06-03 13:55:03 -0500

    adds basic pick up and throw WIP

--- Assets/Scripts/HeadController.cs
+++ Assets/Scripts/HeadController.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour
+public class HeadController : MonoBehaviour
 {
 
     public float speed;
---
+++ Assets/Scripts/PickupHead.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupHead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "HeadIsDetached") {
+			collision.gameObject.tag = "HeadIsAttached";
+			collision.rigidbody.velocity = Vector3.zero;
+			collision.rigidbody.angularVelocity = Vector3.zero;
+			Transform bodyTransform = this.gameObject.transform;
+			Transform headTransform = collision.gameObject.transform;
+			headTransform.Translate (bodyTransform.position.x, bodyTransform.position.y + 0.75f, bodyTransform.position.z);
+			headTransform.parent = bodyTransform;
+		}
+	}
+}
