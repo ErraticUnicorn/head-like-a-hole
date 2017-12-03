32a21422f4e760a3e56fd55571524a5cb1efa4f7
David Wert <david.j.wert@gmail.com>
2017-12-03 10:16:42 -0600

    Begins working on slider

---
+++ Assets/Scripts/PowerSliderBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerSliderBehaviour : MonoBehaviour {
+
+	public Slider slider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
