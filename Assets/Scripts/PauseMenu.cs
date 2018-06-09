1d00e4161bcfb8d8dda47c253ac9744e11d59e4b
David Wert <david.j.wert@gmail.com>
2018-06-09 13:10:28 -0500

    Adds animation

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -39,10 +40,10 @@ public class PauseMenu : MonoBehaviour {
 	}
 
 	public void LoadMenu() {
-
+		Debug.Log ("Loading Menu...");
 	}
 
 	public void QuitGame() {
-
+		Application.Quit ();
 	}
 }
